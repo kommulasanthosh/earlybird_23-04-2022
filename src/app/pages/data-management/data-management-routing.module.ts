2baaa5f8df7d7154e941eb297844a2f991b2798d
NeelimaSharon123 <neelimab97@gmail.com>
2022-04-25 23:15:24 +0530

    Added the common layout for the data management module

--- src/app/pages/data-management/data-management-routing.module.ts
+++ src/app/pages/data-management/data-management-routing.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommonLayoutComponent } from 'src/app/features/common-layout/common-layout.component';
 
 import { DataManagementComponent } from './data-management.component';
 
-const routes: Routes = [{ path: '', component: DataManagementComponent }];
+const routes: Routes = [{
+  path: '', data: { restrict: ['dataManagement'] }, component: CommonLayoutComponent,
+  children: [
+    {
+      path: '', component: DataManagementComponent,
+      children: [
+       
+      ]
+    }
+  ]
+
+ }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
